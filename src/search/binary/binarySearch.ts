418aaf5f053077fa0ad1661f1489ba430a25610d
AlfieRan <89932058+AlfieRan@users.noreply.github.com>
2021-11-25 09:01:05 +0000

    hello

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,8 +1,19 @@
-import { NO_MATCH } from "../common"
+import { NO_MATCH } from "../common";
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 const binarySearch = (items: any[], item: any): number => {
+  return NO_MATCH;
+};
 
-    return NO_MATCH;
+function LetToNum(letter: string) {
+  let num = alphabet.indexOf(letter);
+  return num;
+}
+
+function NumToLet(number: number) {
+  let letter: string = alphabet[number];
+  return letter;
 }
 
 export default binarySearch;
--- src/search/linear/linearSearch.ts
+++ src/search/linear/linearSearch.ts
@@ -1,8 +1,13 @@
 import { NO_MATCH } from "../common";
 
 const linearSearch = (items: any[], item: any): number => {
+  for (let index = 0; index < items.length; index++) {
+    if (items[index] === item) {
+      return index;
+    }
+  }
 
-    return NO_MATCH;
-}
+  return NO_MATCH;
+};
 
 export default linearSearch;
