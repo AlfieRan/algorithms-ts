5ba5b0f0bdbb8e698d2cdf4df11aa135f21aaa4b
AlfieRan <89932058+AlfieRan@users.noreply.github.com>
2021-11-27 11:39:01 +0000

    binary

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,9 +1,48 @@
+import { Console } from "winston/lib/winston/transports";
 import { NO_MATCH } from "../common";
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 const binarySearch = (items: any[], item: any): number => {
-  return NO_MATCH;
+  let completed: boolean = false;
+  let pointer = Math.floor(items.length / 2);
+  console.log(items);
+  let finalPointer = pointer;
+  let editedList = [...items];
+  let result: number = 0;
+  let runs = 0;
+  let maxRuns = 25;
+
+  while (!completed) {
+    runs++;
+    if (runs > maxRuns) {
+      console.log("Infinite loop :(");
+      completed = true;
+    }
+
+    if (item === items[finalPointer]) {
+      result = finalPointer;
+      console.log(result);
+      completed = true;
+    } else if (item < editedList[pointer]) {
+      editedList = editedList.splice(0, pointer);
+      pointer = Math.floor(editedList.length / 2);
+      finalPointer -= pointer;
+    } else if (item > editedList[pointer]) {
+      editedList = editedList.splice(pointer);
+      pointer = Math.floor(editedList.length / 2);
+      finalPointer += pointer;
+    } else {
+      console.log("pain and suffering");
+      return NO_MATCH;
+      completed = true;
+    }
+    console.log(
+      `item:${item}\nlist:${items}\npointer:${pointer}\nfinal-pointer:${finalPointer}\npointer-item:${items[finalPointer]}\nedited-list:${editedList}`
+    );
+  }
+  console.log(`This should be the returned value??? ${result}`);
+  return result;
 };
 
 function LetToNum(letter: string) {
